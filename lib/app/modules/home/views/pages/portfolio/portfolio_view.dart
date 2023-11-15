f5c874e3371e9a81025d32e2a568f1146709689c
Asfaque <asfaque@codersbucket.com>
2023-11-15 17:58:35 +0600

    start of portfolio

---
+++ lib/app/modules/home/views/pages/portfolio/portfolio_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:portfolio/app/modules/home/views/pages/portfolio/portfolio_controller.dart';
+
+class PortfolioView extends StatelessWidget {
+  final PortfolioController controller = Get.put(PortfolioController());
+
+  PortfolioView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
